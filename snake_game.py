51f51c844968545391e2e9288461900b855d728b
Claudio <fanconic@student.ethz.ch>
2018-08-27 23:23:55 +0200

    Added also posionous food, which subtracts points

--- snake_game.py
+++ snake_game.py
@@ -1,6 +1,7 @@
 '''
 Snake game, originally known from old Nokia phones
 The snake is controllable with the arrow keys and is implement as a two dimensional array
+Additionally there are is now aswell poisonous food (marked as a diamond), where you lose pies, if you eat one.
 Author: Claudio Fanconi
 based on: https://www.youtube.com/watch?v=rbasThWVb-c   
 '''
@@ -8,6 +9,9 @@ import random
 import curses
 
 # Initialize Screen and cursor
+# pies is the ammount of pies eaten
+# speedset is a boolean used to increase speed after eating pies
+# Timeout indicates the milliseconds after the screen is refreshed
 pies = 0
 speedset = False
 timeout = 100
@@ -34,9 +38,16 @@ snake = [
     [snake_y, snake_x-2]
 ]
 
-# Create food
+# Create initial food
 food = [screen_height/2, screen_width/2]
 window.addch(int(food[0]),int(food[1]), curses.ACS_PI)
+
+# Generate initial poissonous food
+poison = [
+    random.randint(1,screen_height-1), 
+    random.randint(1,screen_height-1)
+    ]
+window.addch(poison[0], poison[1], curses.ACS_DIAMOND)
 
 # Key actions
 key = curses.KEY_RIGHT
@@ -46,7 +57,7 @@ while True:
     key = key if next_key == -1 else next_key
 
     # Losing the game (if it touches boarders or itself)
-    if snake[0][0] in [0,screen_height] or snake[0][1] in [0,screen_width] or snake[0] in snake[1:]:
+    if snake[0][0] in [0,screen_height-1] or snake[0][1] in [0,screen_width-1] or snake[0] in snake[1:] or pies < 0:
         curses.endwin()
         quit()
 
@@ -82,7 +93,7 @@ while True:
             ]
             
             # Check if food is not created in the snake
-            food = new_food if new_food not in snake else None
+            food = new_food if new_food not in snake or not poison else None
 
         window.addch(food[0], food[1], curses.ACS_PI)
 
@@ -91,6 +102,25 @@ while True:
         window.addch(int(tail[0]), int(tail[1]), ' ')
 
     window.addch(int(snake[0][0]), int(snake[0][1]), curses.ACS_CKBOARD)
+
+    # Snake runs into poisson
+    if snake[0] == poison:
+        pies -= 1
+        speedset = True
+        window.addstr(1, 0, str(pies))
+        poison = None
+        while poison is None:
+
+            # Generate new poison at random new position
+            new_poison = [
+                random.randint(1,screen_height-1),
+                random.randint(1, screen_width -1)
+            ]
+            
+            # Check if food is not created in the snake
+            poison = new_poison if new_poison not in snake or not food else None
+
+        window.addch(poison[0], poison[1], curses.ACS_DIAMOND)
 
     # After eating 5 pies the speed gets 25% faster
     if pies % 5 == 0 and speedset == True:
